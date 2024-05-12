68ffa8f887c45cab53d3af5d8a916846cba4055d
realyashsehgal <yash.sehgal1234@gmail.com>
2024-05-12 21:50:09 +0530

    some leet code and dynamic programming

---
+++ climbingstairs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int climbstairs(int n)
+{
+        int i,curr;
+        int one = 2;
+        int two = 1;
+            if (n <= 1) {
+                return n;
+            }
+        for ( i = 2; i < n; i++) {
+             curr = one + two;
+            two = one;
+            one = curr;
+        }
+        return one;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << climbstairs(n);
+    getch();
+}
